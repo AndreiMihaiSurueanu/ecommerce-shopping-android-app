60f8951c6ed333375621e248943f74b00c4e612f
andrei <git1@email.com>
2021-11-10 20:14:07 +0100

    Display Data in Product Category Recycler View

--- app/src/main/java/com/example/e_commerceshoppingandroidapp/MainActivity.java
+++ app/src/main/java/com/example/e_commerceshoppingandroidapp/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.e_commerceshoppingandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.e_commerceshoppingandroidapp.adapter.ProductCategoryAdapter;
+import com.example.e_commerceshoppingandroidapp.model.ProductCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ProductCategoryAdapter productCategoryAdapter;
+    RecyclerView productCatRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<ProductCategory> productCategoryList = new ArrayList<>();
+        productCategoryList.add(new ProductCategory(1, "Trending"));
+        productCategoryList.add(new ProductCategory(2, "Most Popular"));
+        productCategoryList.add(new ProductCategory(3, "All Body Products"));
+        productCategoryList.add(new ProductCategory(4, "Skin Care"));
+        productCategoryList.add(new ProductCategory(5, "Hair Care"));
+        productCategoryList.add(new ProductCategory(6, "Make Up"));
+        productCategoryList.add(new ProductCategory(7, "Fragrance"));
+
+        setProductRecycler(productCategoryList);
+
+    }
+
+    private void setProductRecycler(List<ProductCategory> productCategoryList){
+
+        productCatRecycler = findViewById(R.id.cat_recycler);
+        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        productCatRecycler.setLayoutManager(layoutManager);
+        productCategoryAdapter = new ProductCategoryAdapter(this, productCategoryList);
+        productCatRecycler.setAdapter(productCategoryAdapter);
+
     }
 }
